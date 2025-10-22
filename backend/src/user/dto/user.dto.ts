38668916cd84b83e621fa097873c5a3dcd953399
Guiziin227 <gh63388@gmail.com>
2025-10-22 10:09:12 -0300

    Criando o dto

--- backend/src/user/dto/user.dto.ts
+++ backend/src/user/dto/user.dto.ts
@@ -1,8 +1,22 @@
+import { IsEmail, IsPhoneNumber, IsTaxId } from 'class-validator';
+
 export class createUserDto {
   firstName: string;
   lastName: string;
+
+  @IsEmail()
   email: string;
+
   password: string;
+
+  @IsPhoneNumber('BR')
   cellphone: string;
+
+  @IsTaxId('pt-BR')
   taxId: string;
+  isPaid: boolean;
+  paidUntil?: string;
+  pixQrCodeId?: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
