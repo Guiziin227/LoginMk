fdb0ec8fd37c47c1f64189edd947dd0d19dc4238
Guiziin227 <gh63388@gmail.com>
2025-10-23 15:23:48 -0300

    Create 0000_friendly_ender_wiggin.sql

---
+++ backend/drizzle/0000_friendly_ender_wiggin.sql
@@ -0,0 +1,15 @@
+CREATE TABLE "users" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "users_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"first_name" varchar(50) NOT NULL,
+	"last_name" varchar(50) NOT NULL,
+	"email" varchar(50) NOT NULL,
+	"password" varchar(75) NOT NULL,
+	"cellphone" varchar(15) NOT NULL,
+	"tax_id" varchar(14) NOT NULL,
+	"is_paid" boolean DEFAULT false NOT NULL,
+	"paid_until" timestamp with time zone,
+	"pix_qr_code_id" varchar(100),
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
